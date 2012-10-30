00dd378315b991ab449614780d0e942397e9331e
Gabriel Forestier <g.forestier@sigire.fr>
2012-10-30 15:34:10 +0100

    Suppression Active Admin

--- config/routes.rb
+++ config/routes.rb
@@ -19,12 +19,12 @@ Crm::Application.routes.draw do
 
 
 
-  ActiveAdmin.routes(self)
+
 
   devise_for :user, :path => '', :path_names => { :sign_in => "login" , :sign_up => "register"},  
   :controllers => { :registrations => "registrations" }
 
-  devise_for :admin_users, ActiveAdmin::Devise.config  
+  # devise_for :admin_users 
 
 
 
