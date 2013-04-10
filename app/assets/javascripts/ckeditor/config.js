c7f162d38ca1b8eb6b51e8df58fc636d43c74079
Manuel OZAN <m.ozan@sigire.fr>
2013-04-10 17:22:45 +0200

    Modifications HTML

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,17 +1,19 @@
 CKEDITOR.editorConfig = function( config )
 {
-    config.bodyClass = 'contents';
+   
+
+	config.extraPlugins = 'autogrow';
+	config.autoGrow_onStartup = true;
+	/*config.bodyClass = 'contents';
+	config.contentsCss = CKEDITOR.basePath + 'contents.css';*/
+	
 	config.toolbar_MyToolbar =
     [
-        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-        { name: 'editing', items : [ 'Scayt' ] },
-        { name: 'colors', items : [ 'TextColor', 'BGColor' ] },
-        { name: 'insert', items : [ 'Image','Table','HorizontalRule','Smiley','SpecialChar' ] },
-        { name: 'links', items : [ 'Link','Unlink' ] },
-                '/',
-        { name: 'styles', items : [ 'Font','FontSize' ] },
+        { name: 'colors', items : [ 'FontSize', 'TextColor', 'HorizontalRule','Smiley','SpecialChar' ] },
         { name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent' ] },
-        { name: 'paragraph', items : [ 'JustifyLeft','JustifyCenter','JustifyRight','-','JustifyBlock' ] }
-    ];
+        { name: 'links', items : [ 'Link','Unlink' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList'] }
+	];
+
 };
+
