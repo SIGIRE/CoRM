846376bb0e8a3496710a05a538236232c7890996
Manuel OZAN <m.ozan@sigire.fr>
2013-04-12 17:01:21 +0200

    Ajout de la vue editer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130411090544) do
+ActiveRecord::Schema.define(:version => 20130411133121) do
+
+  create_table "abilities", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "accounts", :force => true do |t|
     t.string   "company"
