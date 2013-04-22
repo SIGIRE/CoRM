65ed3cf33508743e8d597d9f61484a8de36884da
Manuel OZAN <m.ozan@sigire.fr>
2013-04-22 10:52:09 +0200

    modification html utilisateur et stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,17 +11,25 @@ class HomeController < ApplicationController
   # GET /types
   # GET /types.json
   def index
-    @page = params[:page]
-    # task-block
-    @tasks = Task.where("user_id =? AND statut IN ('En cours','A faire')", current_user.id).order("created_at DESC , updated_at DESC").limit(5)
-    @tasks_count = Task.where("user_id =? AND statut IN ('En cours','A faire')", current_user.id).count() - 5
-    # events-block
-    @events = Event.order('id DESC').limit(6)
-    @opportunities = Opportunity.last_modified(5)
-    @quotations = Quotation.last_modified(5)
-    respond_to do |format|
-      format.html # index.html.erb
-    end
+	if current_user.has_role? :admin 
+	@user_count = User.all_reals.count
+	
+	
+	
+	render "stats"
+	else
+		@page = params[:page]
+		# task-block
+		@tasks = Task.where("user_id =? AND statut IN ('En cours','A faire')", current_user.id).order("created_at DESC , updated_at DESC").limit(5)
+		@tasks_count = Task.where("user_id =? AND statut IN ('En cours','A faire')", current_user.id).count() - 5
+		# events-block
+		@events = Event.order('id DESC').limit(6)
+		@opportunities = Opportunity.last_modified(5)
+		@quotations = Quotation.last_modified(5)
+		respond_to do |format|
+		  format.html # index.html.erb
+		end
+	end
   end
   
   def update
@@ -29,5 +37,7 @@ class HomeController < ApplicationController
 		
 		@events
   end
+  
+  
   
 end
