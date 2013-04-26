ae4f7062b4a588dcc7628b5797fe4a3812c3ae9f
Kelgors <kelgors@gmail.com>
2013-04-26 17:12:18 +0200

    Typeahead mods + migration mods

--- db/migrate/20130426125246_set_tasks_with_no_priority_to_normal.rb
+++ db/migrate/20130426125246_set_tasks_with_no_priority_to_normal.rb
@@ -1,12 +1,12 @@
 class SetTasksWithNoPriorityToNormal < ActiveRecord::Migration
   def up
-    Task.where('priority = NULL').each do |t|
+    Task.where(priority => nil).each do |t|
       t.update_attribute(:priority, 1)
     end    
   end
 
   def down
-    Task.where('priority = 1').each do |t|
+    Task.where(:priority => 1).each do |t|
       t.update_attribute(:priority, nil)
     end
   end
