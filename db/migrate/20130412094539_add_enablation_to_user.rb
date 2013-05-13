a5d7f848b9c7760727e4c99f6a716f41ffa6dc87
Manuel OZAN <m.ozan@sigire.fr>
2013-05-13 17:43:28 +0200

    pbm Deploy Ligépack - correction u.enabled

--- db/migrate/20130412094539_add_enablation_to_user.rb
+++ db/migrate/20130412094539_add_enablation_to_user.rb
@@ -1,6 +1,6 @@
 class AddEnablationToUser < ActiveRecord::Migration
   def change
-    add_column(:users, :enabled, :boolean, { :default => FALSE })
+    add_column(:users, :enabled, :boolean, { :default => false })
     remove_column(:users, :is_admin)
     remove_column(:users, :is_super_user)
   end
