1f75a1bcc6d85d208337fac427f1a8277993274a
Manuel OZAN <m.ozan@sigire.fr>
2013-05-13 17:14:08 +0200

    Modification version gem Money-rails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130513100221) do
+ActiveRecord::Schema.define(:version => 20130423093256) do
 
   create_table "accounts", :force => true do |t|
     t.string   "company"
@@ -233,12 +233,6 @@ ActiveRecord::Schema.define(:version => 20130513100221) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
-
-  create_table "settings", :id => false, :force => true do |t|
-    t.string "key",        :null => false
-    t.string "value"
-    t.string "input_type"
-  end
 
   create_table "tags", :force => true do |t|
     t.string   "name"
