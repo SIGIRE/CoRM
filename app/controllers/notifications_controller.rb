a3f0be3f2d5c87744c4306afb93eae19ff65fbd2
Julien ROHEE <j.rohee@sigire.biz>
2014-02-11 16:54:41 +0100

    Corrections légères

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@
 class NotificationsController < ApplicationController
 
   def index
-		@emails = Email.where("user_id = ?", current_user.id).limit(5).order("send_at")
+		@emails = Email.where("user_id = ?", current_user.id).limit(5).order("id")
 		@tasks = Task.where("user_id =? AND statut IN ('En cours','A faire')", current_user.id).order("statut ASC, priority DESC, term DESC").limit(5)
   end
 
