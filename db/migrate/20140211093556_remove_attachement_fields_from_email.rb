fa4c60371bc00c62ac3d4ecfbd857102c3b66c88
Arthur ROUSSEAU <Arthur@localhost.localdomain>
2014-02-12 16:58:41 +0100

    Modification de la migration pour permettre le rollback
    TODO : Modifier la fonction de traitement (email => evenement)

--- db/migrate/20140211093556_remove_attachement_fields_from_email.rb
+++ db/migrate/20140211093556_remove_attachement_fields_from_email.rb
@@ -1,40 +1,30 @@
 class RemoveAttachementFieldsFromEmail < ActiveRecord::Migration
   def up
-    # Recuperation des emails avec pieces-jointes
-    emails = Email.where(:attach_file_name => !blank?)
-    
     # Suppression des champs devenus inutiles
     change_table :emails do |t|
-        t.remove :attach_file_name, :attach_content_type, :attach_file_size 
+        remove_attachment :emails, :attach
     end
     
     # Creation de la table regroupant les pieces-jointes des emails
     create_table :email_attachements do |t|
         t.integer :email_id
-        t.string :attach_file_name, :attach_content_type
-        t.integer :attach_file_size 
+        t.attachment :attach
         t.timestamps
     end
-    
   end
 
   def down
-    # Recuperation des emails avec pieces-jointes
-    emails = Email.all
-    
-    drop_table :email_attachements
+    # Modification de la table 'emails'
     
     change_table :emails do |t|
-        t.string :attach_file_name, :attach_content_type
-        t.integer :attach_file_size
+        t.attachment :attach
     end
     
-    emails.each do |email|
-        if (!email.email_attachements.blank?)
-            attachements = EmailAttachements.where(:email_id => email.id)
-            attachements.each do |attachement|
-            end
-        end
-    end
+    # Recuperation des emails
+    emails = Email.all
+    
+    # Suppression de la table "email_attachements"
+    drop_table :email_attachements
+    
   end
 end
