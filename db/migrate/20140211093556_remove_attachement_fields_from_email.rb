753060648bd7f1ba3600774c7ab13cc876bd0446
Arthur ROUSSEAU <Arthur@localhost.localdomain>
2014-02-25 14:31:27 +0100

    Modification de certaines migrations

--- db/migrate/20140211093556_remove_attachement_fields_from_email.rb
+++ db/migrate/20140211093556_remove_attachement_fields_from_email.rb
@@ -2,8 +2,12 @@ class RemoveAttachementFieldsFromEmail < ActiveRecord::Migration
   def up
     # Suppression des champs devenus inutiles
     change_table :emails do |t|
+        if !column_exists? :emails, :attach_updated_at
+            add_column :emails, :attach_updated_at, :datetime
+        end
         remove_attachment :emails, :attach
-    end
+	end
+
     
     # Creation de la table regroupant les pieces-jointes des emails
     create_table :email_attachments do |t|
@@ -19,9 +23,6 @@ class RemoveAttachementFieldsFromEmail < ActiveRecord::Migration
     change_table :emails do |t|
         t.attachment :attach
     end
-    
-    # Recuperation des emails
-    emails = Email.all
     
     # Suppression de la table "email_attachments"
     drop_table :email_attachments
