4ee6e181cdf263d1a659a797e32ae890d1bb197a
Thomas NAUDIN <t.naudin@sigire.biz>
2014-04-15 15:02:28 +0200

    Classe Time étendue : to_s(:humanized) renvoie 'Aujourd'hui à', 'Hier à'

---
+++ config/initializers/time_format.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+Time::DATE_FORMATS[:humanized]  = ->(time) do
+  st = Time.now.beginning_of_day
+  nd = Time.now.end_of_day
+
+  case 
+  when time.between?(st + 1.day, nd + 1.day)
+    "Demain à #{time.strftime('%H:%M')}"
+  when time.between?(st, nd)
+    "Aujourd'hui à #{time.strftime('%H:%M')}"
+  when time.between?(st - 1.day, nd - 1.day)
+    "Hier à #{time.strftime('%H:%M')}"
+  else 
+    time.strftime('%d/%m/%y à %H:%M')
+  end
+end
