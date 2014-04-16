099ba3ec8ca07961972593efd5aa222a6d138cbc
Thomas NAUDIN <t.naudin@sigire.biz>
2014-04-16 10:40:08 +0200

    Refonte filtrage des tâches

--- app/models/task.rb
+++ app/models/task.rb
@@ -77,4 +77,5 @@ class Task < ActiveRecord::Base
   scope :by_user_id, lambda { |user_id| where("tasks.user_id = ?", user_id) unless user_id.blank? }
   scope :by_term, lambda { |date_begin,date_end| where("term BETWEEN ? AND ? OR term = ?", date_begin, date_end +'%', '')}
   scope :none, lambda { where('1 = 0') }
+  scope :by_notes_like, lambda { |notes| where("UPPER(tasks.notes) LIKE UPPER(?)", "%#{notes}%") unless notes.blank? }
 end
