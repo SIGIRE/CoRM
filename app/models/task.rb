1dbaf7c2ba0a1393859e9cac63eb7a2304326537
Thomas NAUDIN <t.naudin@sigire.biz>
2014-04-17 11:49:29 +0200

    Correction merge

--- app/models/task.rb
+++ app/models/task.rb
@@ -52,11 +52,6 @@ class Task < ActiveRecord::Base
   # Conservé pour le bon fonctionnement des migrations --> non utilisé
   has_attached_file :attach
   
-  # Nouvelle gestion des pièces-jointes
-  has_many :task_attachments, :dependent => :destroy
-  accepts_nested_attributes_for :task_attachments
-  alias_attribute :attachments, :task_attachments
-  
   Paperclip.interpolates :with_content_type do |attachment, style|
     "#{attachment.instance.with_content_type}"
   end
