6f231df1f3fd50f767b22156acc36c00e0227a08
Thomas NAUDIN <t.naudin@sigire.biz>
2014-05-05 10:25:50 +0200

    Intégration en base d'une notion de langue pour l'utilisateur

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140422082528) do
+ActiveRecord::Schema.define(:version => 20140430154317) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -372,12 +372,12 @@ ActiveRecord::Schema.define(:version => 20140422082528) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",                  :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -386,9 +386,10 @@ ActiveRecord::Schema.define(:version => 20140422082528) do
     t.string   "surname"
     t.string   "tel"
     t.string   "mobile"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "enabled",                :default => false
+    t.datetime "created_at",                                             :null => false
+    t.datetime "updated_at",                                             :null => false
+    t.boolean  "enabled",                             :default => false
+    t.string   "locale",                 :limit => 2
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
