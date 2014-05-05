896a69df130f1e0a6b6b11a1d5f9aac98a3d2d16
Thomas NAUDIN <t.naudin@sigire.biz>
2014-05-05 10:27:16 +0200

    Ajout de la possibilité d'activer/désactiver un compte

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  
   before_filter :getAbility
-  
+  before_filter :get_locale
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
@@ -18,6 +18,15 @@ class ApplicationController < ActionController::Base
     @ability = Ability.new(current_user)
   end
   
+  def get_locale
+    default_locale = I18n.default_locale
+    if current_user
+      I18n.locale = current_user.locale || default_locale
+    else
+      I18n.locale = default_locale
+    end
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_url
   end
