98838ba5ea4e895842ccdac2ff347d41b8e3f817
Thomas Naudin <thomas@naudin.me>
2014-05-20 19:50:38 +0200

    Ajout de contraintes dans le modèle de relation : account1, account2 et name doivent être valorisés pour la création d'une nouvelle relation. Ajout de vérifications de surface dans le formulaire de création de relation.

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -3,11 +3,16 @@
 #
 class Relation < ActiveRecord::Base
   resourcify
+
+  validates :account1, presence: true
+  validates :account2, presence: true
+  validates :name, presence: true
+
   belongs_to :account1, :class_name => 'Account'
   belongs_to :account2, :class_name => 'Account'
   belongs_to :author_user, :foreign_key => 'created_by', :class_name => 'User'
   belongs_to :editor_user, :foreign_key => 'updated_by', :class_name => 'User'
-  
+
   def author
     return author_user || User::default
   end
