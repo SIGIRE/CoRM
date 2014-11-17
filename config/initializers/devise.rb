a6496694a3559756d76e596fd6f294536828689e
Nicolas PLES <n.ples@sigire.biz>
2014-11-17 09:30:59 +0100

    change config.mailer_sender in config/initializers/devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "no-reply@domain.tld"
+  config.mailer_sender = CORM[:mail][:from]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
