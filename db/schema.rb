6c1fc8369e46eb292ef0d9733300380a6bdbbb41
Nicolas PLES <n.ples@sigire.biz>
2014-12-18 15:52:23 +0100

    debug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141217101901) do
+ActiveRecord::Schema.define(:version => 20141210104525) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at", :null => false
