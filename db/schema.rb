24d03fb068df1cb7c5c474b4b96f1aa5b336a97b
Nicolas PLES <n.ples@sigire.biz>
2015-01-07 09:09:15 +0100

    delete category column in imports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150106130556) do
+ActiveRecord::Schema.define(:version => 20150107080343) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -216,7 +216,6 @@ ActiveRecord::Schema.define(:version => 20150106130556) do
   end
 
   create_table "imports", :force => true do |t|
-    t.string   "category"
     t.string   "created_by"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
