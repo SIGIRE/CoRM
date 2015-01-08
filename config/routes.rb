1c8327ea7401d8028ede5630f7dec82220fed538
Nicolas PLES <n.ples@sigire.biz>
2015-01-08 11:31:21 +0100

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -174,14 +174,14 @@ Crm::Application.routes.draw do
   
   # import_accounts routes
   resources :import_accounts
-  match 'importing_accounts', :controller => 'import_accounts', :action => 'importing_accounts'
-  match 'recalculate_duplicates_accounts', :controller=>'import_accounts', :action=>'recalculate_duplicates'
+  match 'import_accounts/importing', :controller => 'import_accounts', :action => 'importing_accounts'
+  match 'import_accounts/recalculate_duplicates', :controller=>'import_accounts', :action=>'recalculate_duplicates'
   match 'destroy_all_invalids_accounts', :controller=>'import_accounts', :action=>'destroy_all_invalids'
   
   #import_contacts routes
   resources :import_contacts
-  match 'importing_contacts', :controller => 'import_contacts', :action => 'importing_contacts'
-  match 'recalculate_duplicates_contacts', :controller=>'import_contacts', :action=>'recalculate_duplicates'
+  match 'import_contacts/importing', :controller => 'import_contacts', :action => 'importing_contacts'
+  match 'import_contacts/recalculate_duplicates', :controller=>'import_contacts', :action=>'recalculate_duplicates'
   match 'destroy_all_invalids_contacts', :controller=>'import_contacts', :action=>'destroy_all_invalids'
   
   # resources :home
