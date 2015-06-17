9961837c298659685f1165dae627e3f39f2d47e2
Gabriel Forestier <g.forestier@sigire.fr>
2015-06-17 09:40:56 +0200

    Display or not business menus based on global settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,5 +5,23 @@ class Setting < ActiveRecord::Base
     has_attached_file :attach, :default_url => "/images/normal/logo-login.png"
     validates_attachment_content_type :attach, :content_type => /\Aimage\/.*\Z/
     
-
+    def self.display_opportunities?
+        setting = Setting.all.first
+        setting.opportunities_display  
+    end
+    
+    def self.display_quotations?
+        setting = Setting.all.first
+        setting.quotations_display  
+    end
+    
+    def self.display_contracts?
+        setting = Setting.all.first
+        setting.contracts_display  
+    end
+    
+    def self.display_business_menu?
+        self.display_opportunities? || self.display_quotations? || self.display_contracts? 
+    end
+    
 end
