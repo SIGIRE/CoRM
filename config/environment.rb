b2fafeaa97183e64e56d626a30ed2e02a3d2492c
Romain Tartière <romain@blogreen.org>
2015-08-14 17:33:38 +0200

    Do not log to stdout.
    The test suite is really hard to read when it's clutterred with a log of
    details.  log/test.log is still available in case of necessity.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Rails.logger = Logger.new(STDOUT)
-
 # Initialize the rails application
 Crm::Application.initialize!
